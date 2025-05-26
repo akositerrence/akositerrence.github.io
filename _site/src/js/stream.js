4d76168de87157b470da6ec07ac780efbbd9b267
Terrence Andre San Gabriel <terrsangabriel@gmail.com>
2025-05-26 09:56:19 -0400

    jekyll config for easier debug

---
+++ _site/src/js/stream.js
@@ -0,0 +1,29 @@
+console.log("main.js loaded");
+
+let curX = 0;
+let curY = 0;
+let tgX = 0;
+let tgY = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const interBubble = document.querySelector(".interactive");
+    if (!interBubble) return;
+
+    function move() {
+        curX = curX + (tgX-curX) / 20;
+        curY = curY + (tgY-curY) / 20;
+
+        const halfW = interBubble.offsetWidth  / 2;
+        const halfH = interBubble.offsetHeight / 2;
+
+        interBubble.style.transform = `translate(${Math.round(curX - halfW)}px, ${Math.round(curY - halfH)}px)`;
+        requestAnimationFrame(move);
+    }
+
+    window.addEventListener("mousemove", (e) => {
+        tgX = e.clientX;
+        tgY = e.clientY;
+    });
+
+    move();
+});
