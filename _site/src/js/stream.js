60defccf59ead88df99648ebb075f1827b0e539a
Terrence Andre San Gabriel <terrsangabriel@gmail.com>
2025-05-28 22:01:14 -0400

    modal test

--- _site/src/js/stream.js
+++ _site/src/js/stream.js
@@ -1,4 +1,4 @@
-console.log("main.js loaded");
+console.log("stream.js loaded");
 
 let curX = 0;
 let curY = 0;
@@ -27,3 +27,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
     move();
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const feed = document.querySelector(".stream-feed");
+    const modal = document.getElementById("post-modal-container");
+    const content = document.getElementById("modal-content");
+    const close = document.getElementById("modal-close");
+
+    feed.addEventListener("click", async (e) => {
+        const wrapper = e.target.closest(".post-wrapper");
+        if (!wrapper) return;
+
+        const post_url = wrapper.dataset.url;
+        if (!post_url) {
+            console.warn("no data-url")
+            return;
+        }
+        e.preventDefault();
+
+        try {
+            const fetched_content = await fetch(post_url);
+            const html = await fetched_content.text();
+            const doc = new DOMParser().parseFromString(html, "text/html");
+            const article = doc.querySelector(".post-modal");
+
+            content.innerHTML = article
+                ? article.innerHTML
+                : "<p>Error loading post</p>";
+
+            modal.style.display = "flex";
+            document.body.style.overflow = "hidden";
+        } catch (err) {
+            modal.style.display = "flex";
+        }
+    })
+
+    close.addEventListener("click", () => {
+        modal.style.display = "none";
+        document.body.style.overflow = "";
+        content.innerHTML = "";
+    });
+
+})
+
