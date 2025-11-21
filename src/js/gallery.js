501f11ff5b370b23867e03e062eeb5be340b4229
Terrence Andre San Gabriel <terrsangabriel@gmail.com>
2025-11-21 01:01:17 -0600

    added portfolio page hover

---
+++ src/js/gallery.js
@@ -0,0 +1,15 @@
+console.log("gallery.js loaded");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const grid = document.querySelector(".project-gallery");
+    if (!grid) {
+        console.warn("No .project-gallery element found");
+        return;
+    }
+
+    const msnry = new Masonry(grid, {
+        itemSelector: ".brick",
+        columnWidth: ".brick",
+        gutter: 10
+    });
+});
